5aa26f761ca93b1028eb944c20a8af30d00c2b4f
Layton4 <Javierete4774@gmail.com>
2021-12-12 23:59:32 +0100

    puliendo un poco

--- Assets/Scripts/changeColor.cs
+++ Assets/Scripts/changeColor.cs
@@ -4,23 +4,18 @@ using UnityEngine;
 
 public class changeColor : MonoBehaviour
 {
-    public Color prefabCol;
+    public Color prefabCol; 
     public Camera cam;
     void Start()
     {
-        prefabCol = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        GetComponent<MeshRenderer>().material.color = prefabCol;
+        prefabCol = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)); //generamos un color random para el gameobject
+        GetComponent<MeshRenderer>().material.color = prefabCol; //se lo asociamos para que tenga un color nada más empezar el juego
     }
-
-    void Update()
-    {
-        
-    }
-
     private void OnMouseDown()
     {
-        cam.GetComponent<Camera>().backgroundColor = prefabCol;
-        prefabCol = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
-        GetComponent<MeshRenderer>().material.color = prefabCol;
+        cam.GetComponent<Camera>().backgroundColor = prefabCol; //accedemos a la cámara y su componente background para poder cambiarle el color por el que hemos clicado
+        prefabCol = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f)); 
+        GetComponent<MeshRenderer>().material.color = prefabCol; //sustituiremos el color del botón que hemos pulsado por uno nuevo random
     }
+   
 }
